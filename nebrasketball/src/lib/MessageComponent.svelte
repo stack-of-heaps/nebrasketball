16e8b531f149f2f08f39773b7ae8f5d9a9655277
stack-of-heaps <37723013+stack-of-heaps@users.noreply.github.com>
2024-01-23 21:49:01 -0600

    Add styling

--- nebrasketball/src/lib/MessageComponent.svelte
+++ nebrasketball/src/lib/MessageComponent.svelte
@@ -1,18 +1,36 @@
 <script lang="ts">
 	import type { Message } from '$lib/types';
+	import { Image, Paper } from '../../node_modules/@svelteuidev/core';
 	export let message = {} as Message;
 </script>
 
-<div>
-	<b>{message.sender}</b>
-	<p class="tiny">{message.time}</p>
-</div>
-<p>{message.content}</p>
-{#if message.reactions}
-	{#each message.reactions as reaction}
-		<p>{reaction.actor} {reaction.reaction}</p>
-	{/each}
-{/if}
+<Paper>
+	<div>
+		<b>{message.sender}</b>
+		<p class="tiny">{message.time}</p>
+	</div>
+	{#if message.photos}
+		<p>Photo URI: {message.photos[0].uri}</p>
+		<Image
+			fit="contain"
+			src={`C:\\Users\\Kyle Karthauser\\Documents\\nebrasketball2\\nebrasketball\\static${message.photos[0].uri}`}
+		/>
+	{/if}
+	{#if message.gifs}
+		<p>GIF URI: {message.gifs[0].uri}</p>
+		<Image
+			fit="contain"
+			src={`C:\\Users\\Kyle Karthauser\\Documents\\nebrasketball2\\nebrasketball\\static${message.gifs[0].uri}`}
+		/>
+	{/if}
+
+	<p>{message.content}</p>
+	{#if message.reactions}
+		{#each message.reactions as reaction}
+			<p>{reaction.actor} {reaction.reaction}</p>
+		{/each}
+	{/if}
+</Paper>
 
 <style>
 	.tiny {
